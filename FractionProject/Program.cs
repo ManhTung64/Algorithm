eacc9747e9ba5860c4dc7b64144bbab8833c4292
ManhTung64 <94000462+ManhTung64@users.noreply.github.com>
2022-08-14 21:47:56 +0700

    Add files via upload

---
+++ FractionProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FractionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f = Utility.InputFraction();
+            Fraction other = Utility.InputFraction();
+            Console.WriteLine(f.Add(other));
+            Console.WriteLine(f);
+            Console.WriteLine(other);
+        }
+    }
+}
